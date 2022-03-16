e73a7490ca025008864fcc033824968d8ea64197
Igor Perekalskiy <perekalskiy_igor@mail.ru>
2022-03-16 21:54:03 +0300

    8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.decorators.cache import cache_page
+# from django.views.decorators.cache import cache_page
 
 from .forms import CommentForm, PostForm
 from .models import Follow, Group, Post, User
